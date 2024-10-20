56c491d6103c8609398fc3d701847ddeb04a3b47
kp <pulverk@gmail.com>
2024-10-20 15:05:19 -0700

    first commit

---
+++ chatrolls.js
@@ -0,0 +1,11 @@
+console.log("EZ Chat Rolls | Loaded");
+
+/*
+
+always try to parse a chat message sent through the text box as a roll
+
+only try if it doesnt start with '/' or something
+
+if it fails, then send the chat message
+
+*/
